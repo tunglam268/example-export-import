6fddf01ed19af657eeec7589e20ca682c49baa26
tunglam268 <73482349+tunglam268@users.noreply.github.com>
2021-11-25 13:19:08 +0700

    Delete account directory

--- account/db-setup.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `account`.`accounts` (
-    `id` INT NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(255) NULL ,
-    `address` VARCHAR(255) NULL,
-    `phonenumber` VARCHAR(255) NOT NULL ,
-    `balance` VARCHAR(255) NOT NULL,
-    PRIMARY KEY (`id`)
-);
--- account/model/model.go
+++
@@ -1,9 +0,0 @@
-package model
-
-type Account struct {
-	Id          int64
-	Name        string
-	Address     string
-	Phonenumber int64
-	Balance     int64
-}
