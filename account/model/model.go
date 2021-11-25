fe62fe1bbad6584d9766cfe0f9ac0cc106798a91
tunglam268 <73482349+tunglam268@users.noreply.github.com>
2021-11-25 14:05:10 +0700

    Delete account directory

--- account/model/model.go
+++
@@ -1,9 +0,0 @@
-package model
-
-type Account struct {
-	Id          int64  `json:"id"`
-	Name        string `json:"name"`
-	Address     string `json:"address"`
-	Phonenumber int64  `json:"phonenumber"`
-	Balance     int64  `json:"balance"`
-}
