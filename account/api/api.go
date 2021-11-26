77deff920e11ae56d861d985e9464be87043d6c7
tunglam268 <73482349+tunglam268@users.noreply.github.com>
2021-11-26 09:49:46 +0700

    Add files via upload

--- account/api/api.go
+++ account/api/api.go
@@ -1,14 +1,13 @@
 package api
 
 import (
-	"database/sql"
-	"fmt"
 	"log"
 	"net/http"
 	"time"
 
 	sp "account.testing.csv/csv"
 	database "account.testing.csv/db"
+	export "account.testing.csv/exportdata"
 )
 
 const batchsize int = 1000
@@ -25,45 +24,50 @@ func ImportFromCSV(res http.ResponseWriter, req *http.Request) {
 		log.Panic(err)
 	}
 	endImport := time.Since(startImport)
-	fmt.Printf("\n Time to read data from CSV file is : %v \n Time to write to DB is : %v \n", endDB, endImport)
+	log.Printf("\n Time to read data from CSV file is : %v \n Time to write to DB is : %v \n", endDB, endImport)
 	res.WriteHeader(http.StatusOK)
 }
 
 func ExportDBToCSV(res http.ResponseWriter, req *http.Request) {
-	db := database.OpenDB()
+	startEx := time.Now()
+	export.ExportData()
+	endEx := time.Since(startEx)
+	log.Printf("Time to export db to csv : %v", endEx)
+	// db := database.OpenDB()
 
-	rows, err := db.Query(fmt.Sprintf("SELECT * from accounts"))
+	// rows, err := db.Query(fmt.Sprintf("SELECT * from accounts"))
 
-	columns, err := rows.Columns()
-	if err != nil {
-		panic(err.Error())
-	}
-	values := make([]sql.RawBytes, len(columns))
-	scanArgs := make([]interface{}, len(values))
-	for i := range values {
-		scanArgs[i] = &values[i]
-	}
-	totalValues := make([][]string, 0)
-	for rows.Next() {
+	// columns, err := rows.Columns()
+	// if err != nil {
+	// 	log.Panic(err.Error())
+	// }
+	// values := make([]sql.RawBytes, len(columns))
+	// scanArgs := make([]interface{}, len(values))
+	// for i := range values {
+	// 	scanArgs[i] = &values[i]
+	// }
+	// totalValues := make([][]string, 0)
+	// for rows.Next() {
 
-		//Save the contents of each line
-		var s []string
+	// 	//Save the contents of each line
+	// 	var s []string
 
-		//Add the contents of each line to scanArgs, and also to values
-		err = rows.Scan(scanArgs...)
-		if err != nil {
-			panic(err.Error())
-		}
+	// 	//Add the contents of each line to scanArgs, and also to values
+	// 	err = rows.Scan(scanArgs...)
+	// 	if err != nil {
+	// 		panic(err.Error())
+	// 	}
 
-		for _, v := range values {
-			s = append(s, string(v))
-			// print(len(s))
-		}
-		totalValues = append(totalValues, s)
-	}
+	// 	for _, v := range values {
+	// 		s = append(s, string(v))
+	// 		// print(len(s))
+	// 	}
+	// 	totalValues = append(totalValues, s)
+	// }
 
-	if err = rows.Err(); err != nil {
-		panic(err.Error())
-	}
-	sp.WriteToCSV(columns, totalValues)
+	// if err = rows.Err(); err != nil {
+	// 	panic(err.Error())
+	// }
+	// sp.WriteToCSV(columns, totalValues)
+	res.WriteHeader(http.StatusOK)
 }
